d7771873f8da11af67314d7fc4625f076a9aa4f6
Jose C Quintas Jr <juniorquintas@gmail.com>
2018-02-18 16:24:53 -0300

    checks and tips on the shellscript

--- new_package.sh
+++ new_package.sh
@@ -3,16 +3,17 @@ package=$1
 component=$2
 
 function packageInput() {
-  echo "What is the Package name?"
+  echo "What is the Package name? (no sir-) (lowercase) (may use -)"
   read package
   package=$(tr '[:upper:]' '[:lower:]' <<< $package)
   package="${package// /-}"
   package="${package//_/-}"
+  package="${package/sir-/''}"
   echo "Package name is: $package"
 }
 
 function componentInput() {
-  echo "What is the Component name?"
+  echo "What is the Component name? (CamelCase)"
   read component
   component=$(tr '[:lower:]' '[:upper:]' <<< ${component:0:1})${component:1}
   echo "Component name is: $component"
@@ -28,14 +29,14 @@ if [[ "$component" = "" ]]; then
   echo
 fi
 
-cp -rv "./template" "./packages/$package"
+cp -rv "./template" "./packages/sir-$package"
 
 sleep 1
 
-sed -i -e "s/COMPONENT_NAME/$component/g" "./packages/$package/index.js"
-sed -i -e "s/COMPONENT_NAME/$component/g" "./packages/$package/index.stories.js"
-sed -i -e "s/COMPONENT_NAME/$component/g" "./packages/$package/index.test.js"
-sed -i -e "s/PACKAGE_NAME/$package/g" "./packages/$package/package.json"
+sed -i -e "s/COMPONENT_NAME/$component/g" "./packages/sir-$package/index.js"
+sed -i -e "s/COMPONENT_NAME/$component/g" "./packages/sir-$package/index.stories.js"
+sed -i -e "s/COMPONENT_NAME/$component/g" "./packages/sir-$package/index.test.js"
+sed -i -e "s/PACKAGE_NAME/$package/g" "./packages/sir-$package/package.json"
 
 echo "Success!"
 echo "Package $package created!"
