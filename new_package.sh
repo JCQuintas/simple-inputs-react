f9da7e9c8f2a5849ab70346eca760ef679896194
Jose C Quintas Jr <juniorquintas@gmail.com>
2018-02-18 13:43:28 -0300

    create templates for packages

---
+++ new_package.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+package=$1
+component=$2
+
+function packageInput() {
+  echo "What is the Package name?"
+  read package
+  package=$(tr '[:upper:]' '[:lower:]' <<< $package)
+  package="${package// /-}"
+  package="${package//_/-}"
+  echo "Package name is: $package"
+}
+
+function componentInput() {
+  echo "What is the Component name?"
+  read component
+  component=$(tr '[:lower:]' '[:upper:]' <<< ${component:0:1})${component:1}
+  echo "Component name is: $component"
+}
+
+if [[ "$package" = "" ]]; then
+  packageInput
+  echo
+fi
+
+if [[ "$component" = "" ]]; then
+  componentInput
+  echo
+fi
+
+cp -rv "./template" "./packages/$package"
+
+sleep 1
+
+sed -i -e "s/COMPONENT_NAME/$component/g" "./packages/$package/index.js"
+sed -i -e "s/COMPONENT_NAME/$component/g" "./packages/$package/index.stories.js"
+sed -i -e "s/PACKAGE_NAME/$package/g" "./packages/$package/package.json"
+
+echo "Success!"
+echo "Package $package created!"
+
+sleep 2
---
+++ template/index.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-color: transparent;
+`
+
+class COMPONENT_NAME extends PureComponent {
+  render() {
+    return <Container>SIR-COMPONENT_NAME</Container>
+  }
+}
+
+COMPONENT_NAME.displayName = 'SIR-COMPONENT_NAME'
+
+COMPONENT_NAME.defaultProps = {}
+
+export default COMPONENT_NAME
